671c6c3b2e0a13350880b457b26bc5db136604cb
lwp <liu_weipinglove@163.com>
2018-05-19 08:49:35 +0800

    新增自定义组件

---
+++ pages/components/bar-ct.js
@@ -0,0 +1,30 @@
+// pages/components/bar-ct.js
+Component({
+  // externalClasses: ['bar-class'],//外部class样式
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    barModel: {//写法为bar-model  
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
